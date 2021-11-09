d5fdcad736529eb692db786ff3c2510aed235db3
ntzwilly <ntzwilly@gmail.com>
2021-11-09 17:37:06 +0200

    Display Missions and use react-bootstrap

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchMissions } from '../redux/missions/missions';
 
-function Missions() {
+const Missions = () => {
   const dispatch = useDispatch();
   const missions = useSelector((state) => state.missions);
 
@@ -13,10 +17,53 @@ function Missions() {
   }, []);
 
   return (
-    <div>
-      Missions
-    </div>
+    <Container>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{'  '}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map(({
+            id, name, description, reserved,
+          }) => (
+            <tr key={id}>
+              <td><p className="fw-bold">{name}</p></td>
+              <td><p>{description}</p></td>
+              <td className="align-middle">
+                {reserved && <Badge bg="info">Active Member</Badge>}
+                {!reserved && <Badge bg="secondary">NOT A MEMBER</Badge>}
+              </td>
+              <td className="col-2 align-middle text-center">
+                {reserved
+                  && (
+                  <Button
+                    size="sm"
+                    variant="outline-danger"
+                  >
+                    Leave Mission
+                  </Button>
+                  )}
+                {!reserved
+                  && (
+                  <Button
+                    size="sm"
+                    variant="outline-secondary"
+                  >
+                    Join Mission
+                  </Button>
+                  )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
-}
+};
 
 export default Missions;
