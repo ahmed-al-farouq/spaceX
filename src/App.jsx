3c9fd6fb40921314f94f9742e4029abe018d4482
Ahmed Al Farouq <mediocre23534@gmail.com>
2021-11-09 12:17:50 +0200

    fix conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Provider } from 'react-redux';
 import {
   BrowserRouter,
   NavLink,
@@ -12,32 +13,34 @@ import Profile from './components/Profile';
 
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <div className="left-section">
-          <img src={logo} alt="logo" className="logo" />
-          <h2>Space Travelers&rsquo; Hub</h2>
-        </div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/" activeClassName="active">Rockets</NavLink>
-            </li>
-            <li>
-              <NavLink to="/missions" activeClassName="active">Missions</NavLink>
-            </li>
-            <li>
-              <NavLink to="/profile" activeClassName="active">My Profile</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Rockets />} />
-        <Route path="missions" element={<Missions />} />
-        <Route path="profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <header>
+          <div className="left-section">
+            <img src={logo} alt="logo" className="logo" />
+            <h2>Space Travelers&rsquo; Hub</h2>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/" activeClassName="active">Rockets</NavLink>
+              </li>
+              <li>
+                <NavLink to="/missions" activeClassName="active">Missions</NavLink>
+              </li>
+              <li>
+                <NavLink to="/profile" activeClassName="active">My Profile</NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="missions" element={<Missions />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
