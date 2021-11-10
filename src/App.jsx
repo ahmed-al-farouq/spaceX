917a50d16e392598778bf3019063d45eccace5ca
Ahmed Al Farouq <mediocre23534@gmail.com>
2021-11-10 10:54:28 +0200

    Edit navbar and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import { useRef } from 'react';
 import { Provider } from 'react-redux';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoClose } from 'react-icons/io5';
 import {
   BrowserRouter,
   NavLink,
@@ -13,6 +16,15 @@ import Profile from './components/Profile';
 import store from './redux/reduxConfig';
 
 function App() {
+  const menuIconRef = useRef();
+  const closeIconRef = useRef();
+  const menuRef = useRef();
+  const handelMenu = () => {
+    menuIconRef.current.classList.toggle('d-none');
+    closeIconRef.current.classList.toggle('d-none');
+    menuRef.current.classList.toggle('open');
+  };
+
   return (
     <Provider store={store}>
       <BrowserRouter>
@@ -21,16 +33,25 @@ function App() {
             <img src={logo} alt="logo" className="logo" />
             <h2>Space Travelers&rsquo; Hub</h2>
           </div>
-          <nav>
+          <div
+            className="menu-icon"
+            ref={menuIconRef}
+          >
+            <GiHamburgerMenu onClick={handelMenu} />
+          </div>
+          <div className="close-icon d-none" ref={closeIconRef}>
+            <IoClose onClick={handelMenu} />
+          </div>
+          <nav ref={menuRef}>
             <ul>
               <li>
-                <NavLink to="/" activeclassname="active">Rockets</NavLink>
+                <NavLink onClick={handelMenu} to="/" activeclassname="active">Rockets</NavLink>
               </li>
               <li>
-                <NavLink to="/missions" activeclassname="active">Missions</NavLink>
+                <NavLink onClick={handelMenu} to="/missions" activeclassname="active">Missions</NavLink>
               </li>
               <li>
-                <NavLink to="/profile" activeclassname="active">My Profile</NavLink>
+                <NavLink onClick={handelMenu} to="/profile" activeclassname="active">My Profile</NavLink>
               </li>
             </ul>
           </nav>
